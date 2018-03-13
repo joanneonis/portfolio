ca6d28b8833fe44c5af63e539cc2338491f1f0a9
joanneonis <joanne.onis@hotmail.com>
2018-03-13 17:40:49 +0100

    snapsvg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { OwlCarousel } from 'ngx-owl-carousel';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('owlElement') owlElement: OwlCarousel;
+  @ViewChild('.owl-stage') el: ElementRef;
+
+  carouselOptions: any = {
+    items: 1,
+    dots: false,
+    navigation: false,
+    autoplay: false,
+    autoplayTimeout: 10000,
+    loop: true,
+    onTranslate: this.onChange.bind(this),
+  };
+
   images = [
     {
       title: 'Uncover',
@@ -32,9 +46,35 @@ export class HomeComponent implements OnInit {
     }
   ];
 
+
+
   constructor() { }
 
   ngOnInit() {
+    const observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutationRecord) {
+          console.log('style changed!');
+      });
+    });
+
+      // const target = this.el.nativeElement;
+
+     // const target: Element = document.getElementById('owl-stage');
+    // observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+  }
+
+  ngAfterViewInit() {
+    console.log(this.el);
+  }
+
+  next() {
+    this.owlElement.next([500]);
+  }
+  prev() {
+    this.owlElement.previous([500]);
+  }
+
+  onChange(e) {
   }
 
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,14 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+// import * as Snap from 'snapsvg';
+// import Snap, { mina } from 'snapsvg-cjs';
 
+declare var Snap: any;
+declare var mina: any;
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
 })
 export class NavComponent implements OnInit {
+  @ViewChild('bg') bg: ElementRef;
+  navOpen = false;
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  open() {
+    const svg = document.getElementById('bg');
+    const s = Snap(svg);
+
+    const step0 = Snap.select('#step0');
+    const step1 = Snap.select('#step1');
+    const step2 = Snap.select('#step2');
+    const step3 = Snap.select('#step3');
+
+    const step0points = step0.node.getAttribute('d');
+    const step1points = step1.node.getAttribute('d');
+    const step2points = step2.node.getAttribute('d');
+    const step3points = step3.node.getAttribute('d');
+
+    const toStep1 = function() {
+      step0.animate({ d: step1points }, 500, mina.linear, toStep2);
+    };
+
+    const toStep2 = function() {
+      step0.animate({ d: step2points }, 500, mina.linear, toStep3);
+    };
+
+    const toStep3 = function() {
+      step0.animate({ d: step3points }, 420, mina.linear, back);
+    };
+
+    const back = function() {
+      step0.animate({ d: step0points }, 420, mina.linear);
+    };
+
+    toStep1();
+    this.navOpen = true;
   }
 
 }
